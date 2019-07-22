234c8ec9d3ab9dbecb6827a2a3164396df479cf8
yuan.ji <is.yuan.ji@gmail.com>
2019-07-23 00:30:27 +0800

    ready to submit

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -15,11 +15,11 @@ class Controller(object):
         self.yaw_controller = YawController(
             wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
-        kp = 0.3
+        kp = 0.5
         ki = 0.1
         kd = 0.0
         mn = 0.0
-        mx = 0.2
+        mx = 0.4
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
         tau = 0.5
